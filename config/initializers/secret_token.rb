552bd27eadfba3d8ee3fa91486faca1b53dc7937
Laila Zaki <laila.circus@gmail.com>
2013-05-06 19:11:59 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd4ac4989b4c93a7bfd3bb4f9223bdbc6c8ee12fbd0e599e5b4ec9970c5a60400219443f2394665cf4407535bfe51fd102a522e70f0eff588071bf5629d8a3a41'
